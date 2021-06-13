0ecc2cbeb994c6e6688c88c9f199d985fef7d308
kiJu2 <thdydehfdl12@gmail.com>
2021-06-13 20:02:38 +0900

    delete `배달` (#31)

--- Miracle/programmers/배달(진행중).js
+++ Miracle/programmers/배달(진행중).js
@@ -1,71 +1,71 @@
-function solution(N, road, K) {
-  const distances = new Array(N).fill(Infinity);
-  distances[0] = 0;
-  const paths = new Array(N)
-                .fill(null)
-                .map(_=> 
-                    new Array(N).fill(null)
-                  );
+// function solution(N, road, K) {
+//   const distances = new Array(N).fill(Infinity);
+//   distances[0] = 0;
+//   const paths = new Array(N)
+//                 .fill(null)
+//                 .map(_=> 
+//                     new Array(N).fill(null)
+//                   );
 
-  road.map(v=>{
-    const nodeA = v[0]-1;
-    const nodeB = v[1]-1;
-    const distance = v[2];
-    const isNull = !paths[nodeA][nodeB];
+//   road.map(v=>{
+//     const nodeA = v[0]-1;
+//     const nodeB = v[1]-1;
+//     const distance = v[2];
+//     const isNull = !paths[nodeA][nodeB];
     
-    function setPath(){
-      paths[nodeA][nodeB] = distance;
-      paths[nodeB][nodeA] = distance;
-    }
+//     function setPath(){
+//       paths[nodeA][nodeB] = distance;
+//       paths[nodeB][nodeA] = distance;
+//     }
 
-    if(isNull){
-      setPath();
-    }
-    else if(paths[nodeA][nodeB] > distance){
-      setPath();
-    }
-  });
+//     if(isNull){
+//       setPath();
+//     }
+//     else if(paths[nodeA][nodeB] > distance){
+//       setPath();
+//     }
+//   });
 
-  const queue = [0];
-  const visited = new Array(N).fill(false);
+//   const queue = [0];
+//   const visited = new Array(N).fill(false);
 
-  while(queue.length > 0){
-    const currentNode = queue.shift();
-    visited[currentNode] = true;
-    const nextNodes = 
-          paths[currentNode]
-          .reduce((acc,cur,idx)=> {
-            if(!!cur){
-              acc.push({
-                node : idx,
-                distance : cur,
-              })
-            }
-            return acc;
-          }, []);
+//   while(queue.length > 0){
+//     const currentNode = queue.shift();
+//     visited[currentNode] = true;
+//     const nextNodes = 
+//           paths[currentNode]
+//           .reduce((acc,cur,idx)=> {
+//             if(!!cur){
+//               acc.push({
+//                 node : idx,
+//                 distance : cur,
+//               })
+//             }
+//             return acc;
+//           }, []);
     
-    nextNodes.map(info=>{
-      const isVisited = visited[info.node];
-      const distance = info.distance;
-      const nextNode = info.node;
+//     nextNodes.map(info=>{
+//       const isVisited = visited[info.node];
+//       const distance = info.distance;
+//       const nextNode = info.node;
 
-      if(!isVisited && !queue.includes(nextNode)){
-        queue.push(nextNode);
-        distances[nextNode] = distance + (distances[currentNode] ? distances[currentNode] : 0);
-      }
-      else{
-        const targetNodeDistance = distances[nextNode];
-        if(targetNodeDistance > (distances[currentNode] + distance)){
-          distances[nextNode] = (distances[currentNode] + distance);
-        }
-      }
-    });
-  }
+//       if(!isVisited && !queue.includes(nextNode)){
+//         queue.push(nextNode);
+//         distances[nextNode] = distance + (distances[currentNode] ? distances[currentNode] : 0);
+//       }
+//       else{
+//         const targetNodeDistance = distances[nextNode];
+//         if(targetNodeDistance > (distances[currentNode] + distance)){
+//           distances[nextNode] = (distances[currentNode] + distance);
+//         }
+//       }
+//     });
+//   }
   
-  return distances.filter(dis=>dis<=K).length;
-}
+//   return distances.filter(dis=>dis<=K).length;
+// }
 
-const test1 = [5,	[[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]],	3];
-const test2 = [6,	[[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]],	4]
-// 0,1,1/ 1,2,3/ 4,1,2/ 0,3,2/ 4,2,1/ 4,3,2
-console.log(solution(...test1));
+// const test1 = [5,	[[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]],	3];
+// const test2 = [6,	[[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]],	4]
+// // 0,1,1/ 1,2,3/ 4,1,2/ 0,3,2/ 4,2,1/ 4,3,2
+// console.log(solution(...test1));
